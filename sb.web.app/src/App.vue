50e40911c3dc5764dcde768ca66dad5942627191
Vincent Ahlström <vinah331@student.liu.se>
2022-02-08 16:52:16 +0100

    Example components

--- sb.web.app/src/App.vue
+++ sb.web.app/src/App.vue
@@ -1,30 +1,16 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
-  <router-view/>
+  <Navbar />
+  <img src="välkommen_banner.png" alt="Välkommen">
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+// @ is an alias to /src
+import Navbar from '@/components/Navbar.vue'
 
-#nav {
-  padding: 30px;
+export default {
+  name: 'Home',
+  components: {
+    Navbar
+  }
 }
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
---
+++ sb.web.app/src/components/Navbar.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="about">
+    <div class="topnav">
+      <a href="#home">Shop</a>
+      <a href="#news">Events</a>
+      <a href="#contact">Lägg upp</a>
+      <a href="#about">Medlemmar</a>
+      <a href="#about">Notiser</a>
+      <a href="#about">Meddelanden</a>
+      <a href="#about">Varukorg</a>
+      <a href="#about">Min Sida</a>
+    </div>
+  </div>
+</template>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+/* Add a black background color to the top navigation */
+.topnav {
+  background-color: #333;
+  overflow: hidden;
+}
+
+/* Style the links inside the navigation bar */
+.topnav a {
+  float: left;
+  color: #f2f2f2;
+  text-align: center;
+  padding: 14px 16px;
+  text-decoration: none;
+  font-size: 17px;
+}
+
+/* Change the color of links on hover */
+.topnav a:hover {
+  background-color: #ddd;
+  color: black;
+}
+
+.about {
+  width: 1442px;
+}
+</style>
