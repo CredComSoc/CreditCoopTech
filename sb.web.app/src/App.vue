62b0cf18db8e0dada753c1e280f7a4366a096e34
kasni325 <kasni325@student.liu.se>
2022-02-15 20:02:03 +0100

    before merge

--- sb.web.app/src/App.vue
+++ sb.web.app/src/App.vue
@@ -2,6 +2,10 @@
   <div id="app">
     <Navbar />
     <Banner />
+    <!-- <ContentCard title="Events" description="Bläddra bland senast upplagda produkter och tjänster." /> -->
+    <ContentCard title="SHOP" description="Bläddra bland senast upplagda produkter och tjänster." theme="yellow-card" theme_btn="yellow-btn" btn_txt="Till shopen" />
+    <ContentCard title="EVENTS" description="Bläddra bland senast upplagda event." theme="blue-card" theme_btn="blue-btn" btn_txt="Till events"  />
+    <ContentCard title="MEDLEMMAR" description="Bläddra bland nya medlemmar i nätverket." theme="yellow-card" theme_btn="yellow -btn" btn_txt="Till medlemmar" />
     <Footer />
   </div>
 </template>
@@ -11,13 +15,15 @@
 import Navbar from './components/Navbar.vue'
 import Banner from '@/components/ContentBanner.vue'
 import Footer from '@/components/Footer.vue'
+import ContentCard from '@/components/ContentCard.vue'
 
 export default {
   name: 'Home',
   components: {
     Banner,
     Footer,
-    Navbar
+    Navbar,
+    ContentCard
   }
 }
 </script>
