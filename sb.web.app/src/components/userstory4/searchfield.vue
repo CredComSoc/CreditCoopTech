146dcb5eca9af0d0e41843fe324b7fe2616f46a7
Robin Jensen <robje408@student.liu.se>
2022-03-10 11:53:11 +0100

    searchfield flexbox
    searchfield flexbox, snyggat till sökrutan också

--- sb.web.app/src/components/userstory4/searchfield.vue
+++ sb.web.app/src/components/userstory4/searchfield.vue
@@ -1,14 +1,12 @@
 <template>
-    <b-col cols="6">
-      <b-form-input class="w-100 d-inline-block" :type="search" v-model="message" placeholder="Vad vill du söka efter idag?"></b-form-input>
-    </b-col>
-    <b-col cols="1">
-      <button class="d-inline-block" @click="$emit('searchEvent', message)">
-        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-          <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
-        </svg>
-      </button>
-    </b-col>
+  <div class="item-center">
+    <input :type="search" v-model="message" placeholder="Vad vill du söka efter idag?"/>
+    <button class="d-inline-block" @click="$emit('searchEvent', message)">
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+        <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+      </svg>
+    </button>
+  </div>
 </template>
 
 <script>
@@ -22,10 +20,27 @@ export default {
 </script>
 <style scoped>
 
+.item-center {
+  display: flex;
+  justify-content: center;
+}
+
+input {
+  width: 30rem;
+  border-radius: 10px 0px 0px 10px;
+  border-color: black;
+}
+
+input:focus{
+  outline: none;
+}
+
 button {
   height: 100%;
   width: 3rem;
-  border-radius: 10px;
+  border-color: black;
+  border-radius: 0px 10px 10px 0px;
+  border-left: transparent;
 }
 
 </style>
