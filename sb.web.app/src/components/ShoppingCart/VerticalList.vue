d0f5388dee26e424554ed2334e5737d32f7eaa9d
kasni325 <kasni325@student.liu.se>
2022-04-08 11:19:36 +0200

    before merge

---
+++ sb.web.app/src/components/ShoppingCart/VerticalList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="cart-list-container">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VerticalList'
+}
+</script>
+
+<style scoped>
+* {
+  font-family: 'Ubuntu' ;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+</style>
