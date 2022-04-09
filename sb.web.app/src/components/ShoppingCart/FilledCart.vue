29fcd215cc48a6391fd89a0f8506701a49965fd1
kasni325 <kasni325@student.liu.se>
2022-04-09 23:58:10 +0200

    cart WIP

--- sb.web.app/src/components/ShoppingCart/FilledCart.vue
+++ sb.web.app/src/components/ShoppingCart/FilledCart.vue
@@ -1,27 +1,66 @@
 <template>
-  <p>Totalt: {{total}} bKr</p>
-  <div type="button" id="confirme">Slutför köp</div>
+  <div id="cart-table">
+    <!-- <CartTableHeader /> -->
+    <div id="table-content">
+      <CartTableRow 
+        v-for="(row,index) in this.cart"
+        :key="row.id"
+        :ind="index + 1"
+        :image="row.image"
+        :title="row.title"
+        :items="row.items"
+        :price="row.price"
+        :sum="row.price * row.items" 
+      />
+    </div>
+  </div>
+  <p>Totalt: {{ total }} bKr</p>
+  <div type="button" id="confirm">Slutför köp</div>
 </template>
 
 <script>
+//import CartTableHeader from './CartTableHeader.vue'
+import CartTableRow from './CartTableRow.vue'
+
 export default {
   name: 'FilledCart',
-  props: ['total']
+  components: {
+    //CartTableHeader,
+    CartTableRow
+  },
+  props: ['total', 'cart']
 }
 </script>
 
 <style scoped>
-    #confirme{
-        right: 0;
-        bottom:0;
-        position:absolute;
-        background-color:#4690CD;
-        color: white;
-        padding: 5px 10px 5px 10px;
-        border-radius: 4px;
-    }
+  *{
+    font-family: 'Ubuntu' ;
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
 
-    #confirme:hover{
-        background: #457EAD;
-    }
+  #confirm{
+      right: 0;
+      bottom:0;
+      position:absolute;
+      background-color:#4690CD;
+      color: white;
+      padding: 5px 10px 5px 10px;
+      border-radius: 4px;
+  }
+
+  #confirm:hover{
+      background: #457EAD;
+  }
+
+  #cart-table {
+    margin-top: 30px;
+    border: solid 1px black;
+  }
+
+  #table-content {
+    min-height: 200px;
+  }
+
 </style>
