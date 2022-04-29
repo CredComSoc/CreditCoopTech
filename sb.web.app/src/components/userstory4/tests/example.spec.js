90896865b50b5e5d875f4024fc48992b5b239964
Robin Jensen <robje408@student.liu.se>
2022-04-29 09:36:36 +0200

    New branch with tests

---
+++ sb.web.app/src/components/userstory4/tests/example.spec.js
@@ -0,0 +1,26 @@
+describe('Example test suite', () => {
+    test('must work', () => {
+      expect(42).toBe(42)
+    })
+    test('must work', () => {
+        expect(42).toBe(42)
+      })
+      test('must work2', () => {
+        expect(42).toBe(42)
+      })
+      test('must wor3k', () => {
+        expect(42).toBe(42)
+      })
+      test('must 4work', () => {
+        expect(42).toBe(42)
+      })
+      test('must5 work', () => {
+        expect(42).toBe(42)
+      })
+      test('must6 work', () => {
+        expect(42).toBe(42)
+      })
+      test('must 7work', () => {
+        expect(42).toBe(42)
+      })
+  })
