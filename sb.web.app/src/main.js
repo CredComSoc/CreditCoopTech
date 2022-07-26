7a2e09d73ab37a0373fcfb71fd4c39aa062e7ae8
Viktor <clausvanship@gmail.com>
2022-07-26 10:13:52 +0200

    temp logo

--- sb.web.app/src/main.js
+++ sb.web.app/src/main.js
@@ -8,10 +8,16 @@ import BootstrapVue3 from 'bootstrap-vue-3'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 import { VueWindowSizePlugin } from 'vue-window-size/option-api'
+import VueHotjar from 'vue-hotjar-next'
 
 const app = createApp(App)
 app.use(store)
 app.use(router)
 app.use(BootstrapVue3)
 app.use(VueWindowSizePlugin)
+
+app.use(VueHotjar, {
+  id: 3081354
+}) 
+
 app.mount('#app')
