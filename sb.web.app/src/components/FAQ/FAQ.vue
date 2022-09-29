a23574612aff47a800643219008a4276ed83f4ef
Pernilla Ljungqvist <pernillaljungqvist@Pernillas-MacBook-Pro.local>
2022-09-29 10:01:25 +0200

    test om FAQ kommer med

--- sb.web.app/src/components/FAQ/FAQ.vue
+++ sb.web.app/src/components/FAQ/FAQ.vue
@@ -3,27 +3,63 @@
   <div>
     <h2 class="center-text">HANDELSREGLER & TIPS</h2>
   </div>
+
+  <div>
+    <h1><b>Ämne 1</b></h1>
+  </div>
     
 </template>
 
-<style>
+<script>
+  
+</script>
 
-* {
+<style scoped>
+  * {
   font-family: 'Ubuntu', sans-serif;
   padding: 0;
   margin: 0;
-}
+  }
 
-.center-text {
+  table {
+    margin-left: auto;
+    margin-right: auto;
+    border-spacing:50px;
+    width: 100%;
+    text-align: center;
+    font-size: 1.2rem;
+  }
+  .center-text {
   text-align: center;
   margin-top: 4rem;
   margin-bottom: 4rem;
   font-size: 2.2rem;
   letter-spacing: 0.3em;
-}
-h2 {
-  margin-top: 2rem;
-  font-size: 3rem;
-}
-
- </style>
+  }
+  
+  h1 {
+  padding: 10px 10px 10px 0px;
+  font-size: 1.6rem;
+  }
+  
+  p {
+    padding: 10px 0px 10px 20px;
+    font-size: 1.2rem;
+  }
+  
+  th {
+    padding: 20px;
+    font-weight: normal;
+  }
+  
+  td {
+    padding: 10px 0px 10px 0px;
+  }
+  
+  .article {
+    align-content: center;
+    display: flex;
+    justify-content: center;
+  }
+  
+  </style>
