216c6f3ea78f812dd7d4c2d30f25d860d173c9c1
Pernilla Ljungqvist <pernillaljungqvist@pernillas-mbp.clients.eduroam.liu.se>
2022-10-03 16:20:04 +0200

    stadgar länkade

--- sb.web.app/src/components/FAQ/Statues.vue
+++ sb.web.app/src/components/FAQ/Statues.vue
@@ -2,11 +2,30 @@
     <div> 
         <h1><b> Stadgar </b></h1>
     </div>
+    <div>
+      <h2>Svensk Barters stadgar finns på följande länk</h2>
+      <a href="https://svenskbarter.se/images/Svensk-barter-stadgar-underskrivna.pdf">Stadgar</a>
+    </div>
 </template>
-    
+<script>
+</script>
 <style scoped>  
         h1 {
           padding: 10px 0px 10px 0px;
           font-size: 1.6rem;
+        }
+        h2 {
+          padding: 10px 0px 10px 0px;
+          font-size: 1.4rem;
+        }
+        h3 {
+          padding: 10px 0px 10px 0px;
+          font-size: 1.2rem;
+          font-weight: bold;
+        }
+        p {
+            padding: 0px;
+            font-size: 1rem;
+            font-weight: normal;
         }
 </style>
