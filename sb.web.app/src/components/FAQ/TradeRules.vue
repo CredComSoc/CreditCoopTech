9b2eccc9eae966558937d0b8179d2a332d997c7f
Pernilla Ljungqvist <pernillaljungqvist@pernillas-mbp.clients.eduroam.liu.se>
2022-10-03 15:26:01 +0200

    All info inne i bakgrund + bild

--- sb.web.app/src/components/FAQ/TradeRules.vue
+++ sb.web.app/src/components/FAQ/TradeRules.vue
@@ -3,7 +3,53 @@
         <h1><b> Handlingsregler </b></h1>
     </div>
     <div> 
-      <a href="https://docs.google.com/document/d/1HD79kcs_IQ_gMn0oGsgESrPbuvDVxd-fYYgYO9xFMvs/edit#">här är länken, vsg</a>
+      <h2><b>ANVÄNDARVILLKOR FÖR DELTAGARE PÅ SVENSKBARTER.SE</b></h2>
+      <h3>1. TILLÄMPNING</h3>
+      <p>1.1. Dessa Användarvillkor gäller för tjänster som tillhandahålls av Svensk Barter Ekonomisk Förening, org.nr 769637-0241 via webbplatsen www.svenskbarter.se (”Svensk Barter”).</p>
+      <p>1.2. Villkoren gäller för alla som ansöker om medlemskap i föreningen, registrerar ett konto på webbplatsen och som deltar i byteshandeln.</p>
+      <h3>2. OM TJÄNSTEN</h3>
+      <p>2.1. Svensk Barter är ett handelskooperativ som också fungerar som internt clearinghouse och tredjeparts bokhållare av byteshandel mellan sina medlemmar, vilka är affärsägare och yrkesverksamma som avtalar med Svensk Barter för att organisera och underlätta byte av sina varor och tjänster med varandra..</p>
+      <p>.2. Medlemmar med säte i Sverige är även delägare i ”Svensk Barter ekonomisk förening” med rösträtt på föreningsstämman.</p>
+      <h3>3. DEFINITIONER</h3>
+      <p>”Medlem” eller ”Kontoinnehavare” är ett legitimt företag eller förening som på frivillig basis vill utbyta varor och / eller tjänster med andra medlemmar och vill teckna sig för Svensk Barters tjänster för bokhantering och byteshandelsmäkleri.</p>
+      <p>”Barteraffär” eller ”Barteravtal” är ett byte, köp eller försäljning av varor och / eller tjänster där betalning sker i Svensk Barters interna kreditsystem registrerat på medlemmens konto enligt dessa regler.</p>
+      <p>”Barterkrona” är en redovisningsenhet som används för att registrera värdet av handeln. Ägarandel i sådana komplementära valutaenheter betecknar rätten att erhålla varor eller tjänster som finns tillgängliga inom bytesringssystemet. Barterkronor får användas endast på det sätt och för det syfte som anges i dessa regler. De kommer inte att betraktas som lagligt betalningsmedel, värdepapper eller råvaror. De får inte lösas in mot kontanter, med undantag för vad som godkänts i dessa regler.</p>
+      <h3>4. ANVÄNDNING AV TJÄNSTEN</h3>
+      <p>4.1. Som Kontoinnehavare ansvarar du för all användning av Tjänsten och för att dina inloggningsuppgifter skyddas mot obehörig åtkomst. Det är ej tillåtet att överlåta eller låta någon annan använda ditt konto.</p>
+      <p>4.2. Webbplatsen är endast till för medlemmar i Svensk Barter eller deltagande användare som fått särskilt godkännande av styrelsen, och får inte användas av andra parter än dessa. Kontoinnehavaren förstår att bristande överensstämmelse med denna bestämmelse kan leda till att kontot avslutas.</p>
+      <p>4.3. Genom att acceptera dessa villkor garanterar Kontoinnehavaren att Tjänsten inte kommer användas för ändamål som strider mot gällande lagstiftning, myndighetsföreskrift eller som på annat sätt kan medföra skada eller olägenhet för Svensk Barter eller tredjeman.</p>
+      <p>4.4. Tjänsten får inte heller användas för att skicka oönskad reklam eller marknadsföring till andra medlemmar. Andra förbjudna aktiviteter inkluderar, men är inte begränsade till, utbredning av maskar, trojaner och virus och användande av nätverket för att tillåta obehörigt tillträde till någon annan tjänst som är tillgänglig via nätverket.</p>
+      <h3>5. MÄKLARTJÄNSTER</h3>
+      <p>5.1. Svensk Barter tillhandahåller tjänster i mäkleri vid organisering och underlättande av handel bland medlemmarna.</p>
+      <p>5.2. Ansvaret för handeln ligger uteslutande hos de två medlemmar som deltar i en affärsuppgörelse inom bytesringssystemet. Svensk Barter kommer att göra sitt bästa för att mäkla varor eller tjänster till de handlande medlemmarna, noggrant registrera och bokföra affärer i barterkronor och att förvalta dessa regler i enlighet med villkoren. Medlemmen bekräftar dock att de enda gällande principerna för all handel inom bytesringssystemet är att ansvaret ligger hos köparen och säljaren, handeln är frivilligt ingiven, att handlande medlemmar inte är medlemmar hos andra handlande medlemmar, att bartermäklare inte deltar via eget företag som medlem, att Svensk Barter inte företräder någon enskild medlem som agent i handeln och inte heller är borgenär för medlemmars negativa saldon i barterkronor. Enda undantaget för att medlemmar kan agera bartermäklare är vid uppstart av en geografiskt ny barterhub och efter styrelsebeslut.</p>
+      <h3>6. TJÄNSTENS TILLGÄNGLIGHET</h3>
+      <p>6.1. Svensk Barter avser att hålla tjänsten tillgänglig alla dagar om året men förbehåller sig rätten att vid underhåll eller uppdateringar stänga ned hela eller delar av tjänsten.</p>
+      <p>6.2. Svensk Barter garanterar inte att användningen av tjänsten kommer vara oavbruten eller fri från fel.</p>
+      <p>6.3. Svensk Barter ansvarar därmed inte för eventuella tekniska fel, oavsett felens orsak, eller andra störningar i tjänstens tillgänglighet som orsakas av till exempel bristfällig internetuppkoppling, störningar hos webbhotellet eller i samband med uppdateringar och underhåll.</p>
+      <h3>7. ANSVARSFRIHET</h3>
+      <p>7.1. Svensk Barter har för avsikt att all information på webbplatsen ska vara korrekt och så uppdaterad som möjligt men detta är ingenting som Svensk Barter på något vis kan garantera.</p>
+      <p>7.2. Svensk Barter ansvarar endast för åtaganden och representationer som skriftligen gjorts av en vederbörligen auktoriserad tjänsteman i Svensk Barter, mäklare, agenter eller anställd, gällande någon som helst skyldighet.</p>
+      <p>7.3. Kontoinnehavaren använder och deltar i tjänsten på egen risk. Svensk Barter är inte ansvarig för eventuella skador eller förluster som är resultatet av Kontoinnehavarens användning eller deltagande i tjänsten.</p>
+      <p>7.4. Svensk Barter ger inga garantier, varken uttryckligt eller underförstått, och frånsäger sig allt ansvar avseende funktion, kvalitet, leveransdatum, säljbarhet, priser eller eventuella villkor för alla handelstransaktioner i bytesringssystemet. Medlemmen förbinder sig härmed att hålla Svensk Barter skadefri med avseende på anspråk, skuld eller ansvarsskyldighet som uppstår genom transaktioner där medlemmen är en köpare eller säljare.</p>
+      <p>7.5. Medlemmen bekräftar vidare att varje handelstransaktion i vilken den deltar, är frivillig och att Svensk Barter varken är sin egen agent eller agent för någon annan medlem i utbyten där Svensk Barter själv inte är en köpare eller säljare.</p>
+      <h3>8. SKADELÖSHETSFÖRBINDELSE</h3>
+      <p>8.1. Drift av denna tjänst medför inneboende risker för Svensk Barter eftersom Svensk Barter bokstavligen inte kan kontrollera alla aspekter av tjänsten hela tiden. Genom att bekräfta detta avtal accepterar du att ersätta Svensk Barter för eventuella skador, kostnader och utgifter, inklusive rimliga juristkostnader, som uppkommer för Svensk Barter till följd av eller som uppstår på grund av din verksamhet på Tjänsten inklusive, men inte begränsat till, eventuella påståenden om brott mot upphovsrätt, varumärke eller skyddat material, eventuella anspråk på skadelidande, förtal, ärekränkning, diskriminering eller liknande, eller eventuell användning av Tjänsten för olagliga ändamål.</p>
+      <h3>9. GEMENSAMT OCH ENSKILT ANSVAR</h3>
+      <p>9.1. Efter godkännande av stadgar, detta avtal och öppnandet av ett handelskonto blir ett bolag eller förening medlem av Svensk Barter, utifrån detta tar medlemmen och den enskilda undertecknaren för medlemmen ut ett gemensamt och separat ansvar för alla inköp och avgifter som anges i detta avtal och den enskilda undertecknaren som verkar på uppdrag av ett bolag eller förening samtycker till att vara personligt ansvarig för full prestering av nämnda bolag eller förening i enlighet med detta avtal.</p>
+      <h3>10. TILLGÄNGLIGHET AV PRODUKTER OCH TJÄNSTER</h3>
+      <p>10.1. Bartersystemet kan bara bestå av ett visst antal varor och tjänster för ett visst antal medlemmar vid varje givet tillfälle.</p>
+      <p>10.2. Svensk Barter ska göra sitt bästa för att ansluta nya företag som medlemmar, vars produkter eller tjänster är önskade av befintliga medlemmar, för att tillgodose alla medlemmars behov. Svensk Barter har åtagit sig att tillhandahålla endast sådana produkter eller tjänster som faktiskt kan vara tillgängliga i bartersystemet vid varje tillfälle.</p>
+      <p>10.3. Medlemmen är införstådd med att en bytesring är en begränsad marknadsplats och att Svensk Barter inte kan uppfylla alla inköpsförfrågningar hela tiden. Svensk Barter är inte ansvarig om en medlem inte kan hitta specifika produkter eller tjänster att köpa från medlemmar.</p>
+      <p>10.4. Svensk Barter åtar sig även att efter bästa förmåga motverka ohållbar deflation i systemet genom att inte ansluta för många medlemmar med samma utbud av varor och tjänster.</p>
+      <h3>11. MARKNADSFÖRING</h3>
+      <p>11.1. Om inte annat överenskommits mellan parterna har Svensk Barter rätten att marknadsföra och annonsera till andra medlemmar om tillgängligheten för medlemmens produkter eller tjänster.</p>
+      <h3>12. PRISER OCH BETALNINGSVILLKOR</h3>
+      <p>12.1. Alla avgifter, inklusive initiala anslutningssavgifter, provisioner och andra avgifter som medlemmarna betalar till Svensk Barter används för att täcka kostnaden för tjänster som görs i behandlingen av medlemmen i bytesringssystemet, drift av clearinghus och mäklarfunktioner, administration av dessa regler, upprättande och underhåll av register och underlättande av medlemmars användning av bytesringssystemet. Överskott från avgifter kan även stadgeenligt investeras i samhällsnytta (Corporate social responsibility).</p>
+      <p>12.2. Medlemmen förbinder sig att betala Svensk Barter samtliga sådana avgifter inom X dagar efter det månatliga kontoutlåtandet, vid sen betalning utgår dröjsmålsränta om x % på det utestående saldot till dess att full betalning erlagts.</p>
+      <p>12.3. Om medlemmen är i dröjsmål med betalning av kontantavgifter i mer än trettio (30) dagar, accepterar medlemmen att avstå från alla sina rättigheter för användning av eventuella positiva saldon i barterkronor på sitt konto och ger Svensk Barter rätten att hålla sådan balans så länge som eventuella kontantavgifter är utestående.</p>
+      <h3>13. SKYLDIGHETER VID ANVÄNDNING AV BARTERKRONOR</h3>
+      <p>13.1. Medlemmen erkänner att barterkronor i bytesringssystemet (positiva saldon) är skulder hos juridiska personer / företag som har spenderat mer än sina intäkter (negativa saldon), att det finns en borgenär / gäldenärsförhållande mellan sådana personer, och att transaktioner med barterkronor som utbytesmedel medför de normala affärsriskerna som är förknippade med en kredittransaktion.</p>
+      <p>13.2. Barterkronor är som regel de skulder som medlemmarna är skyldiga till bytesringssystemet, och inte till Svensk Barter, som enbart är ansvarig för sin egen skuldsättning i bytesringssystemet.</p>
     </div>
 </template>
     
@@ -11,5 +57,19 @@
         h1 {
           padding: 10px 0px 10px 0px;
           font-size: 1.6rem;
+        }
+        h2 {
+          padding: 10px 0px 10px 0px;
+          font-size: 1.4rem;
+        }
+        h3 {
+          padding: 10px 0px 10px 0px;
+          font-size: 1.2rem;
+          font-weight: bold;
+        }
+        p {
+            padding: 0px;
+            font-size: 1rem;
+            font-weight: normal;
         }
 </style>
