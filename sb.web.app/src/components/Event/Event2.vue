7171ca1ade1baba99d574928a0ef684d49aecd97
Alma Wärnsäter <alma.warnsater@liu.se>
2022-10-03 16:46:42 +0200

    Vi har lagt till en plugin-kalender som ram för att kunna lägga till events.

---
+++ sb.web.app/src/components/Event/Event2.vue
@@ -0,0 +1,24 @@
+<script>
+import '@fullcalendar/core/vdom' // solves problem with Vite
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
+  
+export default {
+  components: {
+    FullCalendar // make the <FullCalendar> tag available
+  },
+  data () {
+    return {
+      calendarOptions: {
+        plugins: [dayGridPlugin, interactionPlugin],
+        initialView: 'dayGridMonth'
+      }
+    }
+  }
+}
+
+</script>
+<template>
+  <FullCalendar :options="calendarOptions" />
+</template>
