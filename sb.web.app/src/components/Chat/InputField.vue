0458907bb3a9e8e5d016a41dc670d8f32a8ad9cf
Rojal Maharjan <rojma436@student.liu.se>
2022-11-04 14:06:48 +0100

    changed icon for file upload

--- sb.web.app/src/components/Chat/InputField.vue
+++ sb.web.app/src/components/Chat/InputField.vue
@@ -14,7 +14,9 @@
     <div class="buttons">
       <div class="image-upload">
         <label for="file-input">
-         <img src="https://icons.iconarchive.com/icons/dtafalonso/android-lollipop/128/Downloads-icon.png"/>
+          <div class="file-clip">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M396.2 83.8c-24.4-24.4-64-24.4-88.4 0l-184 184c-42.1 42.1-42.1 110.3 0 152.4s110.3 42.1 152.4 0l152-152c10.9-10.9 28.7-10.9 39.6 0s10.9 28.7 0 39.6l-152 152c-64 64-167.6 64-231.6 0s-64-167.6 0-231.6l184-184c46.3-46.3 121.3-46.3 167.6 0s46.3 121.3 0 167.6l-176 176c-28.6 28.6-75 28.6-103.6 0s-28.6-75 0-103.6l144-144c10.9-10.9 28.7-10.9 39.6 0s10.9 28.7 0 39.6l-144 144c-6.7 6.7-6.7 17.7 0 24.4s17.7 6.7 24.4 0l176-176c24.4-24.4 24.4-64 0-88.4z"/></svg>
+          </div>
         </label>
         <input id="file-input" ref="fileValue" type="file" @change="addFile" class="file-input" accept=".jpg, .png, .jpeg, .pdf, .txt "/>
       </div>
@@ -140,10 +142,6 @@ export default {
   .image-upload>input {
   display: none;
 }
-.image-upload>label>img{
-  width: 30px;
-  cursor: pointer;
-}
 
 .text-area{
   width: 100%;
@@ -189,6 +187,25 @@ height: fit-content;
 }
 .g-can:hover svg path, .g-can:hover {
   background-color: #a60a0a;
+  fill: white;
+}
+
+.file-clip {
+  cursor: pointer;
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #4690CD;
+}
+.file-clip svg {
+  width: 20px;
+  fill: white;
+}
+.file-clip:hover svg path, .file-clip:hover {
+  background-color: #0a60a6;
   fill: white;
 }
 
