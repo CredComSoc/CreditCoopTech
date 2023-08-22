831952fd1753feabb4da57de09ef2082528ee7a9
Miles Thompson <utunga@gmail.com>
2023-08-22 21:01:37 +1200

    Use config to get express_url / backend api URL

--- backend/migrations/0002_add_categories.js
+++ backend/migrations/0002_add_categories.js
@@ -1,77 +1,81 @@
 const fs = require('fs');
 const axios = require('axios').default;
+const config = require('../config')
+const express_url = config.BACK_END_API_URL
 
 const FormData = require('form-data');
 let categories = [
     {
         name: 'food',
-        image_url: './categories_images/060-food.png'
+        image_url: './migrations/categories_images/060-food.png'
     },
     {
         name: 'fiber',
-        image_url: './categories_images/026-cotton.png'
+        image_url: './migrations/categories_images/026-cotton.png'
     },
     {
         name: 'fuel',
-        image_url: './categories_images/028-fossil-fuel.png'
+        image_url: './migrations/categories_images/028-fossil-fuel.png'
     },
     {
         name: 'plants',
-        image_url: './categories_images/031-plants.png'
+        image_url: './migrations/categories_images/031-plants.png'
     },
     {
         name: 'animals',
-        image_url: './categories_images/032-livestock.png'
+        image_url: './migrations/categories_images/032-livestock.png'
     },
     {
         name: 'farm services',
-        image_url: './categories_images/051-agriculture.png'
+        image_url: './migrations/categories_images/051-agriculture.png'
     },
     {
         name: 'farm supplies',
-        image_url: './categories_images/024-supplies.png'
+        image_url: './migrations/categories_images/024-supplies.png'
     },
     {
         name: 'general services',
-        image_url: './categories_images/044-customer-service-1.png'
+        image_url: './migrations/categories_images/044-customer-service-1.png'
     },
     {
         name: 'general products',
-        image_url: './categories_images/043-box.png'
+        image_url: './migrations/categories_images/043-box.png'
     },
     {
         name: 'food',
-        image_url: './categories_images/060-food.png'
+        image_url: './migrations/categories_images/060-food.png'
     },
     {
         name: 'building',
-        image_url: './categories_images/047-brickwall.png'
+        image_url: './migrations/categories_images/047-brickwall.png'
     },
     {
         name: 'fixing',
-        image_url: './categories_images/tool-box.png'
+        image_url: './migrations/categories_images/tool-box.png'
     },
     {
         name: 'healing',
-        image_url: './categories_images/017-healing.png'
+        image_url: './migrations/categories_images/017-healing.png'
     },
     {
         name: 'labor',
-        image_url: './categories_images/055-shovel.png'
+        image_url: './migrations/categories_images/055-shovel.png'
     },
     {
         name: 'education',
-        image_url: './categories_images/012-exchange.png'
+        image_url: './migrations/categories_images/012-exchange.png'
     },
     {
         name: 'knowledge',
-        image_url: './categories_images/011-education.png'
+        image_url: './migrations/categories_images/011-education.png'
     },
     {
         name: 'art',
-        image_url: './categories_images/005-hobby.png'
+        image_url: './migrations/categories_images/005-hobby.png'
     },
 ]
+
+
 let count  = 0;
 categories.forEach(async el => {
     const fileData = fs.createReadStream(el.image_url);
@@ -90,15 +94,20 @@ categories.forEach(async el => {
         credentials: 'include',
     };
 
-    await axios.post('http://127.0.0.1:3000/categories', model, options)
-        .then(response => {
-            count++;
-            console.log('Response:', response.data);
+    await axios.post(express_url + '/categories', model, options)
+        .then((response) => {
+            if (response.status == 200) {
+                count++;
+                console.log(count + ":" + response.status + ": " + response.data)
+            }
+            else {
+                console.log("Error adding category")
+                console.log(count + ":" + response.status + " (" + response.statusText + ") - " + response.data)
+            }
         })
         .catch(error => {
             console.error('Error:', error);
         });
-console.log(count);        
 })
 
 // read me
