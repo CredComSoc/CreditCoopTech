b1178076ec8e426709ea477e7fdd8098c2bd218a
Eugene Muthui <eugenemuthui@Eugenes-MacBook-Pro.local>
2023-11-22 20:32:50 +0300

    Add translation for hardcoded text

--- sb.web.app/src/components/Navbar/Navbar.vue
+++ sb.web.app/src/components/Navbar/Navbar.vue
@@ -152,7 +152,7 @@
           <i class="fa fa-bars"></i>
         </button>
       </nav>
-      <Modal :open="showSignOutModal" :proceedText="$t('nav.confirm_logout')" @proceed="logOut" closeText="No, please go back" @close="showSignOutModal = !showSignOutModal">
+      <Modal :open="showSignOutModal" :proceedText="$t('nav.confirm_logout')" @proceed="logOut" closeText="$t('nav.reject_logout')" @close="showSignOutModal = !showSignOutModal">
         <h4>{{ $t('nav.confirm_sign_out') }}</h4><br />
       </Modal>
     </header>
