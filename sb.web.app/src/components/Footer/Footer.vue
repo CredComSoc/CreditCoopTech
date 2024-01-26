6b447cc39387ad8e6ff1a0cecd67043e0553dbde
Yonas Bekele <yonasbek4@gmail.com>
2024-01-26 09:13:47 +0300

    fix: deployment issue

--- sb.web.app/src/components/Footer/Footer.vue
+++ sb.web.app/src/components/Footer/Footer.vue
@@ -3,7 +3,7 @@
         <div class="footer-container">
             <p class="side-items" id="om-sidan"><router-link :to="{name:'About'}">{{ $t('about') }}</router-link></p>
             <p id="middle-item"><router-link :to="{name:'Home'}">{{ $t('home') }}</router-link></p>
-            <p class="side-items"><a :href="contact_link">{{ $t('contact') }}</a></p>
+            <!-- <p class="side-items"><a :href="contact_link">{{ $t('contact') }}</a></p> -->
         </div>
     </footer>
 </template>
