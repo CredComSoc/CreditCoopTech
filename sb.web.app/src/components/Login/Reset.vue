b7d356aef3fda189fa52270e1885921e4f01d11c
Eugene Muthui <eugenemuthui@Eugenes-MacBook-Pro.local>
2024-03-26 14:35:25 +0300

    Remove commented out code

--- sb.web.app/src/components/Login/Reset.vue
+++ sb.web.app/src/components/Login/Reset.vue
@@ -27,7 +27,6 @@
         <input v-if="!showPassword1" class="box-input" type="password" v-model="password" name="" :placeholder="$t('new_password')" id="password" required>
         <input v-else class="box-input" type="text" v-model="password" name="" :placeholder="$t('new_password')" id="password" required>
         <button type="button" @click="showPasswordToggle('password')" class="password-toggle" >
-          <!-- <span class="password-toggle-icon"><i class="fas fa-eye"></i></span> -->
           <svg v-if="!showPassword1" xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
             <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z"/>
             <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0"/>
@@ -41,7 +40,6 @@
         <input v-if="!showPassword2" class="box-input" type="password" v-model="password2" name="" :placeholder="$t('re_password')" id="re_password" required>
         <input v-else class="box-input" type="text" v-model="password2" name="" :placeholder="$t('re_password')" id="re_password" required>
         <button type="button" @click="showPasswordToggle('re_password')" class="password-toggle" >
-          <!-- <span class="password-toggle-icon"><i class="fas fa-eye"></i></span> -->
           <svg v-if="!showPassword2" xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-eye" viewBox="0 0 16 16">
             <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8M1.173 8a13 13 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5s3.879 1.168 5.168 2.457A13 13 0 0 1 14.828 8q-.086.13-.195.288c-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5s-3.879-1.168-5.168-2.457A13 13 0 0 1 1.172 8z"/>
             <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5M4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0"/>
@@ -244,25 +242,6 @@ button:focus {
 .password-toggle {
   position: relative;
   border-style: hidden;
-}
-.password-toggle-icon {
-  /* position: absolute; */
-  top: 50%;
-  right: 10px;
-  transform: translateY(-50%);
-  cursor: pointer;
-}
-
-.password-toggle-icon i {
-  font-size: 18px;
-  line-height: 1;
-  color: #333;
-  transition: color 0.3s ease-in-out;
-  margin-bottom: 20px;
-}
-
-.password-toggle-icon i:hover {
-  color: #000;
 }
 
 </style>
